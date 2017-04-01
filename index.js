7615ef6bc92c5bfeadb44ee7f3be47af893376ea
eko24ive <eko24ive@gmail.com>
2017-04-01 16:59:46 +0300

    usage of templated url

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var express = require("express");
 var app = express();
 
 
+
 app.get('/', function(req, res) {
     res.send("Hello world !");
 });
+
+app.get('/about', function(req, res) {
+    res.send("This is about page !");
+});
+
+// /book/19/JohnDoe/Something
+app.get('/book/:century/:author/:bookName', function(req, res) {
+    var reqData = req.params;
+    res.send("You've viewing book '" + reqData.bookName + "' written " + reqData.author + " in " + reqData.century + " century ");
+})
 
 app.listen(3000, function() {
     console.log("Server started");
